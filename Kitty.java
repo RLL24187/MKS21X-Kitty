f16d1161a4087e7942e0f64289d8adc2374e3a35
rleongPd5 <rleong00@stuy.edu>
2018-10-03 11:39:01 -0400

    Added constructor in Kitty

---
+++ Kitty.java
@@ -0,0 +1,17 @@
+public class Kitty{
+  private String name;
+  private int age;
+  public Kitty(String StartName, int StartAge) {
+    name = StartName;
+    age = StartAge;
+  }
+
+    //constructor that takes the name and age of the Kitty.
+  public String getName() {
+    return name;
+  }
+  public int getAge() {
+    return age;
+  }
+
+}
